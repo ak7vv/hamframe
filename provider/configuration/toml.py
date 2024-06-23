5c8e5806f6758a41b4cd8e3bdc3737d6d66d6886
Christian Kuhtz <christian@kuhtz.com>
2024-06-23 02:51:37 +0000

    expanding toml module

--- provider/configuration/toml.py
+++ provider/configuration/toml.py
@@ -1,7 +1,17 @@
-def toml_import():
-    print("import.")
+# module provides functions to import toml to redis and export redis to toml
+#
+# logger : defines which logging instance should be used
+# instance_name : prescriptive info as to which instance this configuration table should apply to
+# table_name : prescriptive info as to what table should be written in redis
+#
+
+def toml_import(logger, instance_name, table_name):
+    logger.info("toml_import()")
+    logger.debug("table_name: ", table_name)
     return
 
-def toml_export():
-    print("export.")
+def toml_export(logger, instance_name, table_name):
+    logger.info("toml_import()")
+    logger.debug("table_name: ", table_name)
+
     return
