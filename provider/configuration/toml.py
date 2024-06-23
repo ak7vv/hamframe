c077221bff344149568343dcac6e82d6832dae7b
Christian Kuhtz <christian@kuhtz.com>
2024-06-23 02:55:50 +0000

    more debug

--- provider/configuration/toml.py
+++ provider/configuration/toml.py
@@ -3,13 +3,15 @@
 # logger : defines which logging instance should be used
 # instance_name : prescriptive info as to which instance this configuration table should apply to
 # table_name : prescriptive info as to what table should be written in redis
-#
+# toml_dict : dictionary with toml data for table table_name
 
 
 
-def toml_import(logger, instance_name, table_name):
+def toml_import(logger, instance_name, table_name, **toml_dict):
     logger.info("toml_import()")
+    logger.debug("instance_name: ", instance_name)
     logger.debug("table_name: ", table_name)
+    logger.debug("toml_dict: ", toml_dict)
 
     return
 
@@ -17,6 +19,8 @@ def toml_import(logger, instance_name, table_name):
 
 def toml_export(logger, instance_name, table_name):
     logger.info("toml_import()")
+    logger.debug("instance_name: ", instance_name)
     logger.debug("table_name: ", table_name)
 
-    return
+    logger.debug("toml_dict: ", toml_dict)
+    return toml_dict
