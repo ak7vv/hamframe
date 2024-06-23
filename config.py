656bab977582da9e88b53e332b3ed0e7a6d086bd
Christian Kuhtz <christian@kuhtz.com>
2024-06-23 03:01:35 +0000

    adding logger setup to config.py test

--- config.py
+++ config.py
@@ -4,10 +4,19 @@
 
 import sys
 import os
+import logging
+from systemd.journal import JournalHandler
 
 import provider
 from provider.configuration import *
 
+log = logging.getLogger(__name__)
+log.addHandler(JournalHandler())
+log.setLevel(logging.DEBUG)
+
+log.info("config test start")
+
 provider.configuration.toml_import()
 provider.configuration.toml_export()
 
+log.info("config test complete")
