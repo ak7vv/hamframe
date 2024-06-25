fd76415d7860e3b604c971e104e8cc7cb4f12053
Christian Kuhtz <christian@kuhtz.com>
2024-06-25 21:43:14 +0000

    add args parsing

--- cli/cli.py
+++ cli/cli.py
@@ -1,3 +1,13 @@
 # accepts parameters as commands
 
-print("CLI says 'boo!'.")
+import argparse
+
+parser = argparse.ArgumentParser(description="parse what we're given from docker run")
+parser.add_argument('command', type=str, help="CLI command to be executed")
+parser.add_argument('--file', type=str, help="filename to pass to command")
+
+args = parser.parse_args()
+
+print(args.command)
+if args.file:
+    print(args.file)
