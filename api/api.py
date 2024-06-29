ea4474ff1bf4b3f8821497e18bf309ea0fc6dc34
Christian Kuhtz <christian@kuhtz.com>
2024-06-29 22:48:34 +0000

    reduced import

--- api/api.py
+++ api/api.py
@@ -1,12 +1,14 @@
 # ripped from https://fastapi.tiangolo.com/#example
 
-# from fastapi import FastAPI, Response, status
-from fastapi import *
+from fastapi import FastAPI, Response, status
 
 api = FastAPI()
 
 @api.get("/config/${config_op}", status_code=status.HTTP_200_OK)
-async def config(response: Response, config_op: str, config_section: str | None = None):
+async def config(response: Response, 
+                 config_op: str, 
+                 config_section: str | None = None):
+    
     if config_op == "export":
         return {"config_op": config_op, "config_section": config_section}
     else:
