58bb9660ae420eb11435a6815a976ba7c8846d64
Christian Kuhtz <christian@kuhtz.com>
2024-06-29 23:02:11 +0000

    add mandatory arguments

--- api/api.py
+++ api/api.py
@@ -6,20 +6,25 @@ api = FastAPI()
 
 @api.get("/config/${config_op}", status_code=status.HTTP_200_OK)
 async def config(response: Response, 
-                 config_op: str, 
-                 config_section: str | None = None):
+                 config_op: str,
+                 instance: str,
+                 redis: str, 
+                 config_section: str | None = None ):
     
-    if config_op == "export":
+    if config_op == "export" and instance and redis:
         return {"config_op": config_op, "config_section": config_section}
     else:
         response.status_code = status.HTTP_400_BAD_REQUEST
         return
 
-@api.post("/config/${config_op}")
+@api.post("/config/${config_op}", status_code=status.HTTP_200_OK)
 async def config(response: Response,
                  config_op: str,
-                 config_section: str | None = None):
-    if config_op == "import":
+                 instance: str,
+                 redis: str,
+                 config_section: str | None = None ):
+
+    if config_op == "import" and instance and redis:
         return
     else:
         response.status_code = status.HTTP_400_BAD_REQUEST
