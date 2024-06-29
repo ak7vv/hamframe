4e9443e64758664d1f36cc4df8c901f302a0273a
Christian Kuhtz <christian@kuhtz.com>
2024-06-29 23:02:34 +0000

    tweaks

--- api/api.py
+++ api/api.py
@@ -4,6 +4,8 @@ from fastapi import FastAPI, Response, status
 
 api = FastAPI()
 
+# export configuration
+
 @api.get("/config/${config_op}", status_code=status.HTTP_200_OK)
 async def config(response: Response, 
                  config_op: str,
@@ -16,6 +18,8 @@ async def config(response: Response,
     else:
         response.status_code = status.HTTP_400_BAD_REQUEST
         return
+    
+# import configuration
 
 @api.post("/config/${config_op}", status_code=status.HTTP_200_OK)
 async def config(response: Response,
