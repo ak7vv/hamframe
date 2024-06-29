d5240bce4d4b6da004c87a0d2f7db4e0bd39c314
Christian Kuhtz <christian@kuhtz.com>
2024-06-29 22:44:18 +0000

    response status code

--- api/api.py
+++ api/api.py
@@ -1,12 +1,14 @@
 # ripped from https://fastapi.tiangolo.com/#example
 
-from fastapi import FastAPI
+# from fastapi import FastAPI, Response, status
+from fastapi import *
 
 api = FastAPI()
 
-@api.get("/config/${config_op}")
-async def config(config_op: str, config_section: str | None = None):
+@api.get("/config/${config_op}", status_code=status.HTTP_200_OK)
+async def config(response: Response, config_op: str, config_section: str | None = None):
     if config_op == "export":
         return {"config_op": config_op, "config_section": config_section}
     else:
+        response.status_code = status.HTTP_400_BAD_REQUEST
         return
