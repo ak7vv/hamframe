27eec5228718832b95155fbfb5d60c4ecfa17ed6
Christian Kuhtz <christian@kuhtz.com>
2024-06-29 22:14:24 +0000

    tweak

--- api/api.py
+++ api/api.py
@@ -4,10 +4,6 @@ from fastapi import FastAPI
 
 api = FastAPI()
 
-# @api.get("/")
-# async def read_root():
-#     return {"Hello": "World"}
-
-@api.get("/config/${config_op}/${config_section}")
+@api.get("/config/${config_op}")
 async def config(config_op: str, config_section: str):
     return {"config_op": config_op, "config_section": config_section}
