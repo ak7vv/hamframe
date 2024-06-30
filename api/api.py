603db59d90b1b187a228ccb795f9bce4089a4ff4
Christian Kuhtz <christian@kuhtz.com>
2024-06-30 06:05:38 +0000

    bugfix and wildcard export, optimized wildcard delete

--- api/api.py
+++ api/api.py
@@ -25,17 +25,27 @@ async def config(response: Response,
             return { 'status': 'failure', 'message': 'redis connection failed' }
         # we have a working redis connection
         
-        # construct key and retrieve value
-        key='config:' + instance_param + ':' + config_section
-        value=r.json().get(key, '.')
-        if value:
-            json = { 'value': value }
-            json.update({ 'status': 'success' })
-            json.update({ 'key': key })
+        # construct pattern and retrieve value
+        if config_section:
+            pattern='config:' + instance_param + ':' + config_section
+        else:
+            pattern = 'config:' + instance_param + ':*'
+        
+        keys = r.keys(pattern=pattern)
+        print(keys)
+        if keys:
+            json = {}
+            for key in keys:
+                print(key)
+                value=r.json().get(key, '.')
+                json[len(json)+1] = { 'section': key.split(':')[2], 'value': value }
+            json |= { 'status': 'success', 'message': 'section(s) matching \'' + pattern + '\' in instance' }
+            response.status_code = status.HTTP_200_OK
             return json
         else:
             response.status_code = status.HTTP_404_NOT_FOUND
-            return {'status': 'failure', 'message': 'key \'' + key + '\' does not exist' }
+            return {'status': 'failure', 'message': 'key \'' + pattern + '\' does not exist' }
+
     else:
         response.status_code = status.HTTP_501_NOT_IMPLEMENTED
         return { 'status': 'failure', 'message': 'operation not recognized' }
@@ -76,7 +86,7 @@ async def config(response: Response,
         return { 'status': 'success', 'message': 'key \'' + key + '\' created' }
     
 
-    elif config_op == "delete" and instance_param and redis_param and config_section:
+    elif config_op == "delete" and instance_param and redis_param:
         # do we have a working Redis connection?
         redis_status, r = check_conf_server(redis_param,instance_param)
         if not redis_status:
@@ -84,15 +94,24 @@ async def config(response: Response,
             return { 'status': 'failure', 'message': 'redis connection failed' }
         # we have a working redis connection
 
-        # construct key and delete
-        key='config:' + instance_param + ':' + config_section
-        
-        if r.delete(key, '.'):
-            # response.status_code=status.HTTP_200_OK
-            return { 'status': 'success', 'message': 'key \'' + key + '\' deleted' }
-        else:
-            response.status_code = status.HTTP_404_NOT_FOUND
-            return { 'status': 'failure', 'message': 'key \'' + key + '\' does not exist' }
+        # did we get a specific section to delete?
+        if config_section:
+            pattern = 'config:' + instance_param + ':' + config_section
+        else: # bulk delete
+            pattern = 'config:' + instance_param + ':*'
+
+        cursor = '0'
+        while  cursor != 0:
+            cursor, keys = r.scan(cursor=cursor, match=pattern, count=100)
+            if keys:
+                try:
+                    r.delete(*keys)
+                except:
+                    response.status_code = status.HTTP_502_BAD_GATEWAY
+                    return { 'status': 'failure', 'message': 'redis connection failed' }
+
+        response.status_code = status.HTTP_202_ACCEPTED
+        return { 'status': 'success', 'message': 'no keys matching \'' + pattern + '\' remaining in instance' }
     else:
         response.status_code = status.HTTP_501_NOT_IMPLEMENTED
         return { 'status': 'failure', 'message': 'operation not recognized' }
