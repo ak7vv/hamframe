72e42ad4b4d6e3737b088a9339e4b7f9387c10bc
Christian Kuhtz <christian@kuhtz.com>
2024-07-13 20:35:23 +0000

    fix routers imports

--- api/api.py
+++ api/api.py
@@ -1,6 +1,8 @@
 # implement REST API using FastAPI
 
 from fastapi import FastAPI
+from .routers.configuration.operations import router as configuration_router
+from .routers.database.operations import router as database_router
 
 api = FastAPI()
 
