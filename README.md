32d756480bd8a681d0e190190504a16ea940de9c
Christian Kuhtz <christian@kuhtz.com>
2024-07-13 21:11:40 +0000

    stubs for uvicorn config from within api.py and updated README

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # A Framework for Ham Radio
 
-[![container build status](https://github.com/ak7vv/hamframe/actions/workflows/cli.yml/badge.svg?event=push)](https://github.com/ak7vv/hamframe/actions/)
-[![container build status](https://github.com/ak7vv/hamframe/actions/workflows/api.yml/badge.svg?event=push)](https://github.com/ak7vv/hamframe/actions/)
+![GitHub Actions Workflow Status for CLI build](https://img.shields.io/github/actions/workflow/status/ak7vv/hamframe/cli.yml?branch=main&style=plastic&logo=github&label=CLI%20docker%20build-n-push&labelColor=purple&cacheSeconds=30&link=https%3A%2F%2Fgithub.com%2Fak7vv%2Fhamframe%2Factions%2Fworkflows%2Fcli.yml)
+![GitHub Actions Workflow Status for API build](https://img.shields.io/github/actions/workflow/status/ak7vv/hamframe/api.yml?branch=main&style=plastic&logo=github&label=API%20docker%20build-n-push&labelColor=purple&cacheSeconds=30&link=https%3A%2F%2Fgithub.com%2Fak7vv%2Fhamframe%2Factions%2Fworkflows%2Fapi.yml)
 
 ![GitHub issues current open count](https://img.shields.io/github/issues-search?query=repo%3Aak7vv%2Fhamframe%20state%3Aopen&logo=github&label=current%20open%20issues%20count&color=teal)
 
--- api/api.py
+++ api/api.py
@@ -1,6 +1,9 @@
 # implement REST API using FastAPI
 
+from os import environ
+from time import sleep
 from fastapi import FastAPI
+import uvicorn
 from .routers.configuration.operations import router as configuration_router
 from .routers.database.operations import router as database_router
 from .routers.internal.operations import router as swissarmy_router
@@ -10,3 +13,28 @@ api = FastAPI()
 api.include_router(configuration_router, prefix="/config")
 api.include_router(database_router, prefix="/db")
 api.include_router(swissarmy_router, prefix="/internal")
+
+if __name__ == "__main__":
+
+    redis_host = environ['REDIS_HOST']
+    redis_port = environ['REDIS_PORT']
+
+    if not redis_host:
+        print("REDIS_HOST default not found. Bad container image.")
+        fail = True
+    if not redis_port:
+        print("REDIS_PORT default not found. Bad container image.")
+        fail = True
+
+    if fail:
+        print("Sleeping 5 seconds and exiting.")
+        sleep(5) # slow down restart thrashing
+        exit(1)
+
+    # stubs follow, this should be read from redis kvs for instance, section 'hamframe'
+
+    listener_ip_address = "0.0.0.0"
+    listener_port = "65432"
+    listener_workers = 4
+
+    uvicorn.run("api:app", host=listener_ip_address, port=listener_port, workers=listener_workers)
