3b9fd55eb4608507649f47ff0a8b30c2b5a0d83c
Christian Kuhtz <christian@kuhtz.com>
2024-07-13 20:49:36 +0000

    more refactoring

--- api/api.py
+++ api/api.py
@@ -3,8 +3,10 @@
 from fastapi import FastAPI
 from .routers.configuration.operations import router as configuration_router
 from .routers.database.operations import router as database_router
+from .routers.internal.operations import router as swissarmy_router
 
 api = FastAPI()
 
 api.include_router(configuration_router, prefix="/config")
 api.include_router(database_router, prefix="/db")
+api.include_router(swissarmy_router, prefix="/internal")
---
+++ api/routers/internal/operations.py
@@ -0,0 +1,8 @@
+# Swissarmy (internal) operations
+
+# WARNING:
+# unsupported for operations outside of API itself, do not call from outside of API codebase
+
+from fastapi import APIRouter
+
+router = APIRouter()
