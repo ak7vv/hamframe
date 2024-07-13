dedcefb0cae5c6d6135527dc8b46b099e1850724
Christian Kuhtz <christian@kuhtz.com>
2024-07-13 22:48:03 +0000

    entrypoint bugfix

--- api/api.sh
+++ api/api.sh
@@ -1,13 +1,7 @@
 #!/bin/sh
 
 if [ "${1}" = "dev" ]; then
-	exec fastapi dev api.py \
-		--proxy-headers \
-		--host 0.0.0.0 \
-		--port 8000
+	exec fastapi dev api.py
 else:
-	exec fastapi run api.py \
-		--proxy-headers \
-		--host 0.0.0.0 \
-		--bind 8000
+	exec fastapi run api.py
 fi
