ccbe65d9c36584f650aaeee3c459f1d66c1a91d9
Christian Kuhtz <christian@kuhtz.com>
2024-07-14 19:16:29 +0000

    added logger, renamed listener_ipaddr to listener_host

--- api/api.py
+++ api/api.py
@@ -6,23 +6,28 @@ from time import sleep
 from fastapi import FastAPI
 from contextlib import asynccontextmanager
 import uvicorn
+import logging
 
 from routers.configuration.operations import router as configuration_router
 from routers.database.operations import router as database_router
 from routers.internal.operations import router as swissarmy_router
 
 
+logger = logging.getLogger('uvicorn.error')
 
 @asynccontextmanager
 async def lifespan(api: FastAPI):
     # startup
     check_env_vars()
     api.include_router(configuration_router, prefix='/config')
+    logger.debug('/config route defined')
     api.include_router(database_router, prefix='/db')
+    logger.debug('/db route defined')
     api.include_router(swissarmy_router, prefix='/internal')
+    logger.debug('/internal route defined')
     yield
     # shutdown
-    print(f'bye.')
+    logger.info('bye.')
 
 def check_env_vars():
     required_vars = [ 'REDIS_HOST',
@@ -32,7 +37,8 @@ def check_env_vars():
                       'LISTENER_WORKERS' ]
     for var in required_vars:
         if var not in os.environ:
-            raise EnvironmentError(f'env variable {var} is not set. bad image.')
+            logger.error('env variable {var} is not set. bad image.')
+            exit(1) # bail now
     # we got everything, image is sane
 
 
@@ -43,9 +49,11 @@ if __name__ == '__main__':
 
     # stubs follow, this should be read from redis kvs for instance, section 'hamframe'
 
-    listener_ipaddr = os.environ.get('LISTENER_IPADDR')
+    listener_host = os.environ.get('LISTENER_HOST')
     listener_port = int(os.environ.get('LISTENER_PORT'))
     listener_workers = int(os.environ.get('LISTENER_WORKERS'))
 
+    logger.debug(f'listener: {listener_host}:${listener_port} with {listener_workers} workers.')
+
     # see thread https://github.com/tiangolo/fastapi/issues/1495
-    uvicorn.run(app='__main__:api', host=listener_ipaddr, port=listener_port, workers=listener_workers)
+    uvicorn.run(app='__main__:api', host=listener_host, port=listener_port, workers=listener_workers)
