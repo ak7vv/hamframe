f351146e2d2c26fc755a93c2b258313261c5c56c
Christian Kuhtz <christian@kuhtz.com>
2024-07-16 06:36:25 +0000

    tweak

--- api/api.py
+++ api/api.py
@@ -27,7 +27,8 @@ if __name__ == '__main__':
 
     logger = logging.getLogger(__name__)
     stream_handler = logging.StreamHandler(sys.stdout)
-    log_formatter = logging.Formatter("%(asctime)s [%(processName)s: %(process)d] [%(threadName)s: %(thread)d] [%(levelname)s] %(name)s: %(message)s")
+    # log_formatter = logging.Formatter('%(asctime)s [%(processName)s: %(process)d] [%(threadName)s: %(thread)d] [%(levelname)s] %(name)s: %(message)s')
+    log_formatter = logging.Formatter('%(levelname)s: %(asctime)s - %(message)s')
     stream_handler.setFormatter(log_formatter)
     logger.addHandler(stream_handler)
 
