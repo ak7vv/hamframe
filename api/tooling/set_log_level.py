5d4dc76bb80da433b5122a9c4a9ad7578f1cc05c
Christian Kuhtz <christian@kuhtz.com>
2024-07-16 06:30:49 +0000

    tweaks

--- api/tooling/set_log_level.py
+++ api/tooling/set_log_level.py
@@ -1,9 +1,16 @@
-import logging
+from logging import Logger, _nameToLevel
 
 def set_log_level(logger: Logger, level_str: str) -> None:
+    """
+    Sets logging level for a provided Logger
+
+    Args:
+        logger (Logger): reference to an existing Logger
+        level_str (str): case-insensitive text string for logging level, e.g. 'debug' for LOG_DEBUG.
+    """
     level_str = level_str.upper()
 
-    if level_str in logging._nameToLevel:
-        logger.setLevel(logging._nameToLevel[level_str])
+    if level_str in _nameToLevel:
+        logger.setLevel(_nameToLevel[level_str])
     else: # if the name got borked, default to debug
-        logger.setLevel(logging._nameToLevel['DEBUG'])
+        logger.setLevel(_nameToLevel['DEBUG'])
