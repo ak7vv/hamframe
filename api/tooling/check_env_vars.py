e8f1fbe751f396aee4a2904dd729e39a2c082b1b
Christian Kuhtz <christian@kuhtz.com>
2024-07-16 05:40:08 +0000

    tweak debug

--- api/tooling/check_env_vars.py
+++ api/tooling/check_env_vars.py
@@ -36,12 +36,12 @@ def check_env_vars(logger: Logger) -> Dict:
 
     for var in vars:
         if var not in environ:
-            logger.critical(f'env variable {var} is not set, set{var} = \'{var_defaults[var]}\'')
+            logger.critical(f'{var} = \'{var_defaults[var]}\' (default)')
             # The problematic exiting of fastapi/uvicorn/gunicorn and restart behavior means
             # we will populate anything not specified in required with a sane default and leave it
             # to the user to override if they have other ideas of what's sane.  Bailing is not an option.
             env[var] = var_defaults[var]
         else:
-            logger.info(f'env variable {var} = \'{environ(var)}\'')
+            logger.info(f'{var} = \'{environ(var)}\'')
             env[var] = environ(var)
     return env
