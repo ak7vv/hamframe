a97313664c64addeeab478cd04f2896fbc4e2cc9
Christian Kuhtz <christian@kuhtz.com>
2024-07-16 22:13:45 +0000

    tweak

--- api/routers/configuration/get.py
+++ api/routers/configuration/get.py
@@ -1,6 +1,6 @@
 # Get Configuration operation
 
-from ...tooling.logger import logger
+from ...tooling.logger_init import logger
 from fastapi import Request, Response, status, Query
 from ..database.redis import check_conf_server
 
