04c1bd94e6e2225ee8a8d65bac7b41ccb70477ac
Christian Kuhtz <christian@kuhtz.com>
2024-07-23 05:59:09 +0000

    tweak

--- api/routers/configuration.py
+++ api/routers/configuration.py
@@ -1,8 +1,14 @@
 # Configuration operations
 
+from enum import Enum
 from fastapi import APIRouter
+import redis
 
 router = APIRouter()
+
+class SectionName(str, Enum):
+    n0nbh = 'n0nbh'
+    couchbase = 'couchbase'
 
 @router.get('/configuration')
 async def get_configuration():
@@ -13,5 +19,9 @@ async def get_configuration_instance(instance: str):
     return {'instance': instance}
 
 @router.get('/configuration/{instance}/{section}')
-async def get_configuration_instance_section(instance: str, section: str):
-    return {'instance': instance}, {'section': section}
+async def get_configuration_instance_section(instance: str, section: SectionName):
+    match section:
+        case SectionName.couchbase:
+            return {'instance': instance}, {'message': 'I am a futon.'}
+        case SectionName.n0nbh:
+            return {'instance': instance}, {'message': 'It is dark.'}
