1da528a2aa303c679acf6464c2143a483250f494
Christian Kuhtz <christian@kuhtz.com>
2024-07-23 05:45:24 +0000

    typed

--- api/routers/configuration.py
+++ api/routers/configuration.py
@@ -9,9 +9,9 @@ async def get_configuration():
     return {'instance': 'instance'}
 
 @router.get('/configuration/{instance}')
-async def get_configuration_instance(instance):
+async def get_configuration_instance(instance: str):
     return {'instance': instance}
 
 @router.get('/configuration/{instance}/{section}')
-async def get_configuration_instance_section(instance, section):
+async def get_configuration_instance_section(instance: str, section: str):
     return {'instance': instance}, {'section': section}
