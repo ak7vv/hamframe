2092c1e702661a6833c57e4c7b4b835535e3dd19
Christian Kuhtz <christian@kuhtz.com>
2024-07-25 22:57:23 +0000

    indent fix

--- api/globals.py
+++ api/globals.py
@@ -1,7 +1,7 @@
 
 env = {}
-    """This Dict is set by internal.check_env_vars and contains a subset of the environment relevant to this
-    application.  If a value isn't provided in the environment, check_env_vars will populate a reasonable
-    default.  In turn, components of this application can refer to env[] to retrieve global settings to
-    bootstrap. 
-    """
+"""This Dict is set by internal.check_env_vars and contains a subset of the environment relevant to this
+application.  If a value isn't provided in the environment, check_env_vars will populate a reasonable
+default.  In turn, components of this application can refer to env[] to retrieve global settings to
+bootstrap. 
+"""
